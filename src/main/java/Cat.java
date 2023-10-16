74f7983caa1ada27422327419207869ba5e0b5c1
Rimasta <Renicheronte@yandex.ru>
2023-10-16 16:24:21 +0500

    Added equals, hashCode, toString methods for Cat, HalloWorld classes

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,9 +1,8 @@
+import java.util.Objects;
+
 public class Cat {
 
     private String message;
-
-    public Cat() {
-    }
 
     public String getMessage() {
         return message;
@@ -11,5 +10,27 @@ public class Cat {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+
+        return Objects.equals(message, cat.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return message != null ? message.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "message='" + message + '\'' +
+                '}';
     }
 }
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class HelloWorld {
 
     private String message;
@@ -10,4 +12,25 @@ public class HelloWorld {
         this.message = message;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        HelloWorld that = (HelloWorld) o;
+
+        return Objects.equals(message, that.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return message != null ? message.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorld{" +
+                "message='" + message + '\'' +
+                '}';
+    }
 }
